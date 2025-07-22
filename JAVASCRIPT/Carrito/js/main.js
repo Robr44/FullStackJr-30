cb53b21817b3e1b70fd33f39c339c54046979862
Roberto Mangandi <master495@outlook.es>
2025-07-22 11:32:02 -0600

    Aplicando funcionalidad a carrito de Cursos en archivo HTML Carrit/index.html mediante JS

--- JAVASCRIPT/Carrito/js/main.js
+++ JAVASCRIPT/Carrito/js/main.js
@@ -1,1 +1,52 @@
-console.log("Holas");
+let cursosCarrito = [];
+const contenedorCarrito = document.getElementById('cuerpo-carrito');
+
+function vaciarCarrito(event) {
+    console.log("Carrito vaciado");
+}
+
+function agregarCurso(evento) {
+    console.log("Curso agregado al carrito");
+    // console.log(event.target.parentElement.parentElement);
+    let curso = leerDatosCurso(evento.target.parentElement.parentElement);
+    console.log(curso);
+    cursosCarrito.push(curso);
+    console.log(cursosCarrito);
+    pintarCarritoHTML();
+}
+
+function leerDatosCurso(curso) {
+    console.log(curso);
+    console.log(curso.querySelector('a').getAttribute('data-id'));
+    console.log(curso.querySelector('img').src);
+    console.log(curso.querySelector('h4').textContent);
+    console.log(curso.querySelector('h5').textContent);
+
+    const infoCurso = {
+        id: curso.querySelector('a').getAttribute('data-id'),
+        imagen: curso.querySelector('img').src,
+        nombre: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('h5').textContent,
+        cantidad: 1
+    }
+    return infoCurso;
+
+}
+
+function pintarCarritoHTML(){
+    cursosCarrito.map((curso) => {
+        //crear una fila
+        const fila = document.createElement('tr');
+
+        //asignar los valores en celdas
+        fila.innerHTML= `
+            <td><img src="${curso.imagen}"></td>
+            <td>${curso.nombre}</td>
+            <td>${curso.precio}</td>
+            <td>${curso.cantidad}</td>
+            <td><a class="btn btn-danger">Eliminar</a></td>
+        `
+        contenedorCarrito.appendChild(fila);
+    })
+}
+pintarCarritoHTML()
