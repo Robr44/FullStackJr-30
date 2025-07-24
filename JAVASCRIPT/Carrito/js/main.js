b56a3d7f1ec9f0a288902b3f5c7d372a016c3213
Roberto Mangandi <master495@outlook.es>
2025-07-23 18:55:42 -0600

    continuacion del carrito

--- JAVASCRIPT/Carrito/js/main.js
+++ JAVASCRIPT/Carrito/js/main.js
@@ -3,6 +3,9 @@ const contenedorCarrito = document.getElementById('cuerpo-carrito');
 
 function vaciarCarrito(event) {
     console.log("Carrito vaciado");
+
+    cursosCarrito = [];
+    contenedorCarrito.innerHTML = "";
 }
 
 function agregarCurso(evento) {
@@ -10,7 +13,24 @@ function agregarCurso(evento) {
     // console.log(event.target.parentElement.parentElement);
     let curso = leerDatosCurso(evento.target.parentElement.parentElement);
     console.log(curso);
+
+    // chequeamos si el curso existe previamente y guardamos true o false
+    const existe = cursosCarrito.some( (cursoArr) => cursoArr.id === curso.id)
+
+    if(existe){
+        cursosCarrito.map( (cursoArr) => {
+            if(cursoArr.id === curso.id){
+                cursoArr.cantidad += 1;
+
+                //Aumentar el precio
+                // Utilizar un metodo string que pueda quitar el primer caracter
+                cursoArr.precio += cursoArr.precio.substring(1);
+                return cursoArr;
+            }
+        })
+    }else{
     cursosCarrito.push(curso);
+    }
     console.log(cursosCarrito);
     pintarCarritoHTML();
 }
@@ -34,13 +54,16 @@ function leerDatosCurso(curso) {
 }
 
 function pintarCarritoHTML(){
+    // Limpiar el carrito de compras
+    contenedorCarrito.innerHTML="";
+
     cursosCarrito.map((curso) => {
         //crear una fila
         const fila = document.createElement('tr');
 
         //asignar los valores en celdas
         fila.innerHTML= `
-            <td><img src="${curso.imagen}"></td>
+            <td><img src="${curso.imagen}" width="80" ></td>
             <td>${curso.nombre}</td>
             <td>${curso.precio}</td>
             <td>${curso.cantidad}</td>
@@ -49,4 +72,4 @@ function pintarCarritoHTML(){
         contenedorCarrito.appendChild(fila);
     })
 }
-pintarCarritoHTML()
+pintarCarritoHTML();
