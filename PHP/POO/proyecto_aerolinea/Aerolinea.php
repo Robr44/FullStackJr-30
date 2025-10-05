05edc111a1243d2d4faa869f2f0a81a0008ec526
Roberto <master495@outlook.es>
2025-10-05 15:07:42 -0600

    Aplicacion de Persistencia en proyecto de aerolineas

--- PHP/POO/proyecto_aerolinea/Aerolinea.php
+++ PHP/POO/proyecto_aerolinea/Aerolinea.php
@@ -1,74 +1,82 @@
-<?php
-    class Aerolinea{
-        private $nombre;
-        private $cant_aviones;
-        private $tipo_aerolinea;
+<?php 
 
-        function __construct($nombreParam, $cant_avionesParam, $tipo_aerolineaParam){
-            $this->nombre = $nombreParam;
-            $this->cant_aviones = $cant_avionesParam;
-            $this->tipo_aerolinea = $tipo_aerolineaParam;
-        }
+class Aerolinea{
+    private $nombre;
+    private $cant_aviones;
+    private $tipo_aerolinea;
+    private $id;
 
-       /**
-         * Get the value of nombre
-         */ 
-        public function getNombre()
-        {
-                return $this->nombre;
-        }
+    function __construct($idParam,$nombreParam, $cant_avionesParam, $tipo_aerolineaParam)
+    {
+        $this->id = $idParam;
+        $this->nombre = $nombreParam;
+        $this->cant_aviones = $cant_avionesParam;
+        $this->tipo_aerolinea = $tipo_aerolineaParam;        
+    }
+    
 
-        /**
-         * Set the value of nombre
-         *
-         * @param string $nombre
-         */ 
-        public function setNombre($nombre)
-        {
-                $this->nombre = $nombre;
+    /**
+     * Get the value of nombre
+     */ 
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
 
-                return $this;
-        }
-
-        /**
-         * Get the value of cant_aviones
-         */ 
-        public function getCant_aviones()
-        {
-                return $this->cant_aviones;
-        }
-
-        /**
-         * Set the value of cant_aviones
-         *
-         * @param number $cant_aviones
-         */ 
-        public function setCant_aviones($cant_aviones)
-        {
-                $this->cant_aviones = $cant_aviones;
-
-                return $this;
-        }
-
-        /**
-         * Get the value of tipo_aerolinea
-         */ 
-        public function getTipo_aerolinea()
-        {
-                return $this->tipo_aerolinea;
-        }
-
-        /**
-         * Set the value of tipo_aerolinea
-         *
-         * @param string $tipo_aerolinea
-         */ 
-        public function setTipo_aerolinea($tipo_aerolinea)
-        {
-                $this->tipo_aerolinea = $tipo_aerolinea;
-
-                return $this;
-        }
+    /**
+     * Set the value of nombre
+     *
+     * @param string $nombre
+     */ 
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
 
     }
+
+    /**
+     * Get the value of cant_aviones
+     */ 
+    public function getCant_aviones()
+    {
+        return $this->cant_aviones;
+    }
+
+    /**
+     * Set the value of cant_aviones
+     * @param number $cant_aviones
+     */ 
+    public function setCant_aviones($cant_aviones)
+    {
+        $this->cant_aviones = $cant_aviones;
+    }
+
+    /**
+     * Get the value of tipo_aerolinea
+     * 
+     */ 
+    public function getTipo_aerolinea()
+    {
+        return $this->tipo_aerolinea;
+    }
+
+    /**
+     * Set the value of tipo_aerolinea
+     *
+     * @param string $tipo_aerolinea
+     */ 
+    public function setTipo_aerolinea($tipo_aerolinea)
+    {
+        $this->tipo_aerolinea = $tipo_aerolinea;
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
 ?>
--- PHP/POO/proyecto_aerolinea/index.php
+++ PHP/POO/proyecto_aerolinea/index.php
@@ -1,31 +1,54 @@
-<?php
+<?php 
 print_r($_POST);
+require_once './Aerolinea.php';
 
-// Llamamos a la clase Aerolinea para crear un objeto
+// $_SESSION -> Variable reservada para almacenar datos (Array assoc)
+//INICIAMOS LA SESION PARA PODER UTILIZAR LA VARIABLE $_SESSION
+
+session_start();
+
+print_r($_SESSION);
+
+//Llamamos la clase Aerolineas para crear un objeto
 // include -> Incluir el archivo y si no existe, mostrar un error y continuar la ejecucion del codigo
 // require -> Requerir el archivo y si no existe, mostrar un error y detener la ejecucion del codigo
 
-// include_once -> Incluir el archivo una sola vez, si se vuelve a llamar dentro del archivo, va a usar la misma refencia
-// require_once -> Requerir el archivo una sola vez, si se vuelve a llamar dentro del archivo, va a usar la misma refencia
+// include_once -> Incluir el archivo una sola vez, si se vuelve a llamar dentro de este archivo, va a usar la misma referencia
+// require_once -> Requerir el archivo una sola vez, si se vuelve a llamar dentro de este archivo, va a usar la misma referencia
 
-require_once './Aerolinea.php';
 
-$aerolineas = [];
 
-if(isset($_POST['nombre_aerolinea'],$_POST['cantidad_aviones'],$_POST['tipo_aerolinea'])){
-  
+// Persistencia de datos
+// Auxiliar para prechequear session
+if(!isset($_SESSION['aerolineas'])){
+    $_SESSION['aerolineas'] = [];
+}
+
+
+$aerolineas = $_SESSION['aerolineas'];
+
+
+if(isset($_POST['nombre_aerolinea'], $_POST['cantidad_aviones'], $_POST['tipo_aerolinea']) ){
+
+    $id = rand(1,1000);
     $nombre = $_POST['nombre_aerolinea'];
     $cant_aviones = $_POST['cantidad_aviones'];
     $tipo_aero = $_POST['tipo_aerolinea'];
-
-    $aerolineacita = new Aerolinea($nombre, $cant_aviones, $tipo_aero);
-
+    
+    $aerolineacita = new Aerolinea($id,$nombre,$cant_aviones,$tipo_aero);
+    
     print_r($aerolineacita);
-    array_push($aerolineas, $aerolineacita);
-    echo "<h1>aerolineas:</h1> <br> ";
-    print_r($aerolineas);
+    array_push($aerolineas,$aerolineacita);
+    //$aerolineas[] = $aerolineacita;
+
+    // $_SESSION['aerolineas'][] = $aerolineas; otra forma de pushear los datos
+    $_SESSION['aerolineas'] = $aerolineas;
+
+    echo "<h1>Aerolineas hasta ahora</h1><br>"; 
+    print_r($_SESSION['aerolineas']);
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -33,26 +56,50 @@ if(isset($_POST['nombre_aerolinea'],$_POST['cantidad_aviones'],$_POST['tipo_aero
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Practica CRUD Aerolineas</title>
 </head>
-<body style="background-color: gray">
-    <h1>¡¡Bienvenido a aerolineas!!</h1>
+<body style="background-color: gray;" >
+    <h1>Holiwis bienvenido a Aerolineas Roberto</h1>
 
     <h3>Crear una nueva aerolinea</h3>
     <form action="" method="POST">
         <label for="nombre_aerolinea">Nombre Aerolinea: </label>
         <input type="text" name="nombre_aerolinea" required>
-        
-        <label for="cantidad_aviones">Cantidad Aviones: </label>
-        <input type="number" name="cantidad_aviones" required>
 
-        <label for="tipo_aerolinea">Tipo de Aerolinea</label>
-        <select type="text" name="tipo_aerolinea" required>
-            <option value="">--Selecciona--</option>
+        <label for="nombre_aerolinea">Cantidad Aviones: </label>
+        <input type="text" name="cantidad_aviones" required>
+
+        <label for="nombre_aerolinea">Tipo de Aerolinea: </label>
+        <select type="text" name="tipo_aerolinea">
             <option value="Privado">Privado</option>
             <option value="Comercial">Comercial</option>
             <option value="Carga">Carga</option>
             <option value="Nacional">Nacional</option>
         </select>
         <button type="submit">Crear</button>
+
     </form>
+
+    <main>
+        <table>
+            <thead>
+                <th># ID</th>
+                <th>Nombre</th>
+                <th>Cantidad de aviones</th>
+                <th>Tipo Aerolinea</th>
+            </thead>
+            <tbody>
+                <?php 
+                    foreach($aerolineas as $aero){
+                        echo "<tr>
+                            <td>{$aero->getId()}</td>
+                            <td>{$aero->getNombre()}</td>
+                            <td>{$aero->getCant_aviones()}</td>
+                            <td>{$aero->getTipo_aerolinea()}</td>
+                        </tr> ";
+                    }
+                ?>
+            </tbody>
+
+        </table>
+    </main>
 </body>
 </html>
